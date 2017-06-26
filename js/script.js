e37b3e00aca82be227800e7adea3e730a824e21e
i323940 <nithin.davis@sap.com>
2017-06-27 00:13:56 +0530

    a bit better

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
-var inputElement = document.getElementById("input");
-inputElement.addEventListener("change", handleFiles, false);
-function handleFiles() {
+// var inputElement = document.getElementById("input");
+// inputElement.addEventListener("change", handleFiles, false);
+// function handleFiles() {
+//   if(this.files){
+//     let fileList = this.files[0];
+//     let previewElement = document.querySelector("#preview");
+//     previewElement.setAttribute("src",URL.createObjectURL(fileList));
+//   }
+// }
+
+document.getElementById("input1").addEventListener("change", setImage, false);
+document.getElementById("input2").addEventListener("change", setImage, false);
+document.getElementById("input3").addEventListener("change", setImage, false);
+document.getElementById("input4").addEventListener("change", setImage, false);
+document.getElementById("input5").addEventListener("change", setImage, false);
+
+function setImage() {
   if(this.files){
-    var fileList = this.files[0];
-    var previewElement = document.querySelector("#preview");
-    previewElement.setAttribute("src",URL.createObjectURL(fileList));
+    this.parentElement.setAttribute("style",("background-image: url(" + (URL.createObjectURL(this.files[0])) + ");"));
   }
 }
